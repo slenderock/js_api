9bfe7eb35708e7e021d2a192a0d7adc05f39765e
slenderock <sergiy20101995@ukr.net>
2016-12-18 03:04:48 +0200

    yoda thinking

--- public/js/yoda.js
+++ public/js/yoda.js
@@ -1,21 +1,23 @@
 $(document).ready(function () {
-
     $("#btnTrigger").on('click', function () {
-
+        $("#loading-bro").show();
         $.ajax({
             url: 'https://yoda.p.mashape.com/yoda', // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
             type: 'GET', // The HTTP Method
             data: {sentence: $("#boxArea").val()}, // Additional parameters here
             datatype: 'json',
             success: function (data) {
+                $("#loading-bro").hide();
                 alert(data);
             },
             error: function (err) {
+                $("#loading-bro").hide();
                 alert(err);
             },
             beforeSend: function (xhr) {
                 xhr.setRequestHeader("X-Mashape-Authorization", "MXEjIWfQmOmshh33Fh8IENN7oU7up14GWbGjsn71It7mRvRCGp"); // Enter here your Mashape key
             }
         });
+
     });
 });
